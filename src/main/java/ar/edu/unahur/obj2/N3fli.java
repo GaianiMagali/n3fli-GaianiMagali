f335fbb1983c6f0298fcf32c837945c9e39aa8a4
Darío Hruszecki <dhruszecki@despegar.com>
2019-04-16 08:58:38 -0300

    commit inicial

---
+++ src/main/java/ar/edu/unahur/obj2/N3fli.java
@@ -0,0 +1,8 @@
+package ar.edu.unahur.obj2;
+
+public class N3fli {
+
+    public static void main(String[] args) {
+        System.out.println("N3fli!");
+    }
+}
---
+++ src/test/java/ar/edu/unahur/obj2/N3fliTest.java
@@ -0,0 +1,10 @@
+package ar.edu.unahur.obj2;
+
+import static org.testng.Assert.*;
+
+public class N3fliTest {
+
+    @org.testng.annotations.Test
+    public void testMain() {
+    }
+}
